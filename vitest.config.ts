fe9fec336397eda70baf5a9ad0e28bc8a076ea8f
tigeregg <thetigeregg@gmail.com>
2026-02-19 13:33:38 +0100

    Enforce 80% coverage minimum

--- vitest.config.ts
+++ vitest.config.ts
@@ -22,8 +22,8 @@ export default defineConfig({
       reporter: ['text', 'html'],
       thresholds: {
         statements: 80,
-        branches: 73,
-        functions: 84,
+        branches: 80,
+        functions: 80,
         lines: 80
       }
     }

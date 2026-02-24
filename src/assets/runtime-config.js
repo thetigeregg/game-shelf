154ea57557995cc672ef5b9a06c128a0a4577f4b
tigeregg <thetigeregg@gmail.com>
2026-02-24 16:38:12 +0100

    Limit desktop fullscreen modals

--- src/assets/runtime-config.js
+++ src/assets/runtime-config.js
@@ -2,7 +2,7 @@ window.__GAME_SHELF_RUNTIME_CONFIG__ = Object.assign(
   {},
   window.__GAME_SHELF_RUNTIME_CONFIG__,
   {
-    appVersion: "0.1.25",
+    appVersion: "0.1.31",
     featureFlags: {
       showMgcImport: false,
     },

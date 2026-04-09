2ec83e9c010a2bc47024ad80c951185f97d7a65c
tigeregg <thetigeregg@gmail.com>
2026-04-09 17:12:42 +0200

    Revert "fix(emulator): update allowed attributes for iframe"
    This reverts commit f2af855186f9eb6af87023056088ef8d69af2438.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,12 @@
+import { EMULATORJS_PINNED_PATH_TO_DATA } from '../app/core/config/emulatorjs.constants';
+
 export const environment = {
   production: true,
   gameApiBaseUrl: '/api',
   manualsBaseUrl: '/manuals',
   romsBaseUrl: '/roms',
   biosBaseUrl: '/bios',
-  emulatorJsPathToData: '/assets/emulatorjs/data/',
+  emulatorJsPathToData: EMULATORJS_PINNED_PATH_TO_DATA,
   emulatorJsLoaderIntegrity:
     'sha384-CwARP2ej7UlPGk5E0IPt89lxjdb3t7zStyLR6PL7Sg4xzHSrvXh/R4vbb4PrSv6U',
   emulatorJsDebug: false,

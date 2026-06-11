e527c6a81210a1bd1cab1471ae2dfb7ec5dba537
tigeregg <thetigeregg@gmail.com>
2026-06-11 17:01:53 +0200

    feat(config): add Angular package filtering to ncu configuration

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,6 +1,18 @@
 const base = require('@thetigeregg/ncu-config');
 
+const ANGULAR_MAJOR = 21;
+
+const isAngularPackage = (name) =>
+  name.startsWith('@angular/') || name.startsWith('@angular-eslint/');
+
 module.exports = {
   ...base,
   reject: (name) => name === 'typescript',
+  filterResults: (packageName, { upgradedVersionSemver }) => {
+    if (!isAngularPackage(packageName)) {
+      return true;
+    }
+
+    return parseInt(upgradedVersionSemver?.major, 10) === ANGULAR_MAJOR;
+  },
 };

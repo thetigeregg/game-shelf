92b1b382165e849928af366d5d0d3a4a816c62a4
tigeregg <thetigeregg@gmail.com>
2026-06-12 11:08:32 +0200

    fix(ios): remove unnecessary completion handler call in remote notification

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -57,6 +57,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         NotificationCenter.default.post(name: Notification.Name.init("didReceiveRemoteNotification"), object: completionHandler, userInfo: userInfo)
-        completionHandler(.noData)
     }
 }
